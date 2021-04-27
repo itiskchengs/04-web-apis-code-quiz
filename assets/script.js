617261b2d27f4954b27c0a8c164c207f3f75e9f1
Kenneth Cheng <itskcheng@gmail.com>
2021-04-26 20:09:15 -0400

    Added to the html page and styles to the css page

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+//When you click the start button  --> DOM eventlisterner that listens for a click and runs a function.
+//It starts the quiz -> Function that starts the quiz
+//It also starts the timer  -> Create a variable that runs the timer. In the quiz function take one second off the timer using the set time out.
+//When you answer it correct display on the bottom that you answered correctly and move on to the next question (bonus you can add like 5 seconds to the timer if you get it right)
+//When you answer it wrong display on the bottom that you answered it incorrectly and move to the next question and also take time off the timer. 
+//When you answer all the questions or the timer reaches 0 the game is over. 
+//When the game is over It shows All done your final score is and then the score of the amount they won.
+//Then you can write in your initials and the score and your initials is saved to the highscore list. -> Save it to local storage. 
+//When you click on highscore shows the highscore of the people that played. -> Use Local storage with Json stringify and Json Parse to get back the object. Then probably yse dot notation to grab the data.
+//On the highscore page 
+//When you click submit your score and initals it will bring you to the highscore screen where you can go back to the start quiz screen or clear your highscore 
+//The clear your highscore will clear the highscore from the highscore page.
+
+
